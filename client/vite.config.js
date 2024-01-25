961338be7bf28d2829d5955538af2bbe25934c7f
Camille Ivins <camillemivins@gmail.com>
2024-01-25 12:16:09 -0700

    push test

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'tower-project',
+  base: '',
   build: {
     outDir: 'docs',
     sourcemap: false
